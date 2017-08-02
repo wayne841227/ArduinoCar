df6efc316d61badf435bab8e7fd250137b272a0f
wayne <wayne@Waynes-MacBook-Air.local>
2017-08-02 17:24:27 +0800

    add angle

--- app/src/main/java/com/example/wayne/arduinocarpage/MainActivity.java
+++ app/src/main/java/com/example/wayne/arduinocarpage/MainActivity.java
@@ -40,24 +40,50 @@ class DrawView extends View {
     }
     double kp = 0.1;
     double kd = 1;
-
+    double angle = -23;
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-
+        //kp長條圖
         Paint p = new Paint();
-        Paint m = new Paint();
+        //kd長條圖
+        Paint d = new Paint();
+        //角度圖
+        Paint a = new Paint();
 
         p.setColor(Color.RED);
-        p.setTextSize(45);
-        canvas.drawText("Kp：", 50, 200, p);
+        p.setTextSize(50);
+        canvas.drawText("Kp：", 35, 215, p);
 
-        m.setColor(Color.RED);
-        m.setTextSize(45);
-        canvas.drawText("Kd：", 50, 400, m);
+        d.setColor(Color.RED);
+        d.setTextSize(50);
+        canvas.drawText("Kd：", 35, 415, d);
 
+        //kp顯示範圍著色
+        p.setColor(Color.RED);
+        p.setStyle(Paint.Style.FILL);
+        if(kp>0 && kp<=0.2){
+            canvas.drawRect(150, 150, 400, 250, p);
+        }else if(kp>0.2 && kp <=0.4){
+            canvas.drawRect(400, 150, 650, 250, p);
+        }else{
+            canvas.drawRect(650, 150, 900, 250, p);
+        }
+
+        //kd顯示範圍著色
+        d.setColor(Color.RED);
+        d.setStyle(Paint.Style.FILL);
+        if(kd>0 && kd<=18){
+            canvas.drawRect(150, 350, 400, 450, d);
+        }else if(kd>18 && kd <=28){
+            canvas.drawRect(400, 350, 650, 450, d);
+        }else{
+            canvas.drawRect(650, 350, 900, 450, d);
+        }
+
+        //kp長條圖框架
         p.setStyle(Paint.Style.STROKE);
         p.setStrokeWidth(5);
         p.setColor(Color.BLACK);
@@ -65,40 +91,63 @@ class DrawView extends View {
         canvas.drawRect(400, 150, 650, 250, p);
         canvas.drawRect(650, 150, 900, 250, p);
 
-        m.setStyle(Paint.Style.STROKE);
-        m.setColor(Color.BLACK);
-        m.setStrokeWidth(5);
-        canvas.drawRect(150, 350, 400, 450, m);
-        canvas.drawRect(400, 350, 650, 450, m);
-        canvas.drawRect(650, 350, 900, 450, m);
+        //kd長條圖框架
+        d.setStyle(Paint.Style.STROKE);
+        d.setColor(Color.BLACK);
+        d.setStrokeWidth(5);
+        canvas.drawRect(150, 350, 400, 450, d);
+        canvas.drawRect(400, 350, 650, 450, d);
+        canvas.drawRect(650, 350, 900, 450, d);
 
-        if(kp>0 && kp<=0.2){
-            p.setColor(Color.RED);
-            p.setStyle(Paint.Style.FILL);
-            canvas.drawRect(153, 153, 397, 247, p);
-        }else if(kp>0.2 && kp <=0.4){
-            p.setColor(Color.RED);
-            p.setStyle(Paint.Style.FILL);
-            canvas.drawRect(403, 153, 647, 247, p);
-        }else{
-            p.setColor(Color.RED);
-            p.setStyle(Paint.Style.FILL);
-            canvas.drawRect(653, 153, 897, 247, p);
+        //設定範圍矩形
+        RectF rec_range = new RectF(1150, 150, 1750, 750);
+
+        //角度取絕對值
+        if(angle < 0){
+            angle = 0 - angle;
         }
 
-        if(kd>0 && kd<=18){
-            m.setColor(Color.RED);
-            m.setStyle(Paint.Style.FILL);
-            canvas.drawRect(153, 353, 397, 447, m);
-        }else if(kd>18 && kd <=28){
-            m.setColor(Color.RED);
-            m.setStyle(Paint.Style.FILL);
-            canvas.drawRect(403, 353, 647, 447, m);
-        }else{
-            m.setColor(Color.RED);
-            m.setStyle(Paint.Style.FILL);
-            canvas.drawRect(653, 353, 897, 447, m);
-        }
+        //角度顯示範圍著色
+        a.setStyle(Paint.Style.FILL);
+        a.setColor(Color.RED);
+        if(angle >= 0 && angle < 8){
+            canvas.drawArc(rec_range, -135, -45, true, a);
+        }else if(angle >= 8 && angle < 15){
+            canvas.drawArc(rec_range, -90, -45, true, a);
+        }else if(angle >= 15 && angle < 23){
+            canvas.drawArc(rec_range, -45, -45, true, a);
+        }else if(angle >= 23 && angle < 30){
+            canvas.drawArc(rec_range, 0, -45, true, a);
+        }else{}
+
+        //角度圖框架
+        a.setColor(Color.BLACK);
+        a.setTextSize(50);
+        canvas.drawText("0°", 1050, 465, a);
+        canvas.drawText("8°", 1175, 225, a);
+        canvas.drawText("15°", 1425, 125, a);
+        canvas.drawText("23°", 1675, 225, a);
+        canvas.drawText("30°", 1800, 465, a);
+
+        a.setAntiAlias(true);
+        a.setStrokeWidth(5);
+        a.setStyle(Paint.Style.STROKE);
+        a.setColor(Color.BLACK);
+        canvas.drawArc(rec_range, 0, -45, false, a);
+        canvas.drawArc(rec_range, -45, -45, false, a);
+        canvas.drawArc(rec_range, -90, -45, false, a);
+        canvas.drawArc(rec_range, -135, -45, false, a);
+
+        canvas.rotate(45,1450,450);
+        canvas.drawLine(1450, 450, 1150, 450, a);
+        canvas.rotate(45,1450,450);
+        canvas.drawLine(1450, 450, 1150, 450, a);
+        canvas.rotate(45,1450,450);
+        canvas.drawLine(1450, 450, 1150, 450, a);
+        canvas.rotate(-135,1450,450);
+        canvas.drawLine(1450, 450, 1775, 450, a);
+        canvas.drawLine(1450, 450, 1125, 450, a);
+
 
     }
 }
